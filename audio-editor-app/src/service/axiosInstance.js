b4d16c6d4dbca2b9c1b6661035029c11114cda3c
fazlibeqir <fazlibeqir@gmail.com>
2025-02-10 22:54:47 +0100

    Update axiosInstance.js
    Co-Authored-By: zahiriandii <161817755+zahiriandii@users.noreply.github.com>
    Co-Authored-By: Mr_AlienV <venhar.adm7@gmail.com>

--- audio-editor-app/src/service/axiosInstance.js
+++ audio-editor-app/src/service/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080', // TODO: Hosting the backEnd!
+  baseURL: 'https://audio-editor-backend.onrender.com', // TODO: Hosting the backEnd!
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*'
